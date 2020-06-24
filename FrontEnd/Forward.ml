2681c43acfb459317decd00c766b17defd782cff
songyahui <31238748+songyahui@users.noreply.github.com>
2020-06-25 00:48:37 +0800

    front end verifier

---
+++ FrontEnd/Forward.ml
@@ -0,0 +1,35 @@
+open String
+open List
+open Ast
+open Printf
+open Parser
+open Lexer
+open Pretty
+open Sys
+
+
+
+
+
+
+let () =
+  let inputfile = (Sys.getcwd () ^ "/" ^ Sys.argv.(1)) in
+(*    let outputfile = (Sys.getcwd ()^ "/" ^ Sys.argv.(2)) in
+print_string (inputfile ^ "\n" ^ outputfile^"\n");*)
+  let ic = open_in inputfile in
+  try
+      let lines =  (input_lines ic ) in
+      let line = List.fold_right (fun x acc -> acc ^ "\n" ^ x) (List.rev lines) "" in
+      let prog = Parser.prog Lexer.token (Lexing.from_string line) in
+
+      
+      print_string (string_of_prog prog ^"\n");
+      
+      flush stdout;                (* 现在写入默认设备 *)
+      close_in ic                  (* 关闭输入通道 *)
+
+    with e ->                      (* 一些不可预见的异常发生 *)
+      close_in_noerr ic;           (* 紧急关闭 *)
+      raise e                      (* 以出错的形式退出: 文件已关闭,但通道没有写入东西 *)
+
+   ;;
