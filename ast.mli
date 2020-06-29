5ccfa876255f93736579bf5cdc2902b3a2d1dddc
songyahui <31238748+songyahui@users.noreply.github.com>
2020-06-29 12:39:48 +0800

    changed the ast

--- ast.mli
+++ ast.mli
@@ -1,39 +1,80 @@
-
-
-type state = One | Zero 
-type signal = (string * state)
-type var = string
+type var = string  (*name of the signal e.g., A B C*)
 type name = string
 
+
+type state = One | Zero
+type mapping = (var * state)
+
+
 (*signal set*)
-type ss = signal list * signal list 
-(*前面的是constrain， 后面的是signal*)
+type instance = mapping list * mapping list 
+           (*前面的是constrain,  后面的是signal assignment*)
+
 
 type es = Bot 
         | Emp 
-        | Instance of ss 
-        | Or of es * es 
+        | Instance of instance 
         | Con of es * es
         | Kleene of es
 
-type terms = Var of var   
-           | Number of int 
-           | Plus of terms * terms (*n+1*)
-           | Minus of terms * terms     
+type history = es 
 
-type pure = TRUE
-          | FALSE
-          | Gt of terms * terms
-          | Lt of terms * terms
-          | GtEq of terms * terms
-          | LtEq of terms * terms
-          | Eq of terms * terms
-          | PureOr of pure * pure
-          | PureAnd of pure * pure
-          | Neg of pure
+type current = ss
+
+type trace = history * current 
+
+type precondition = var list * trace 
 
 
-type effect = (pure * es) list  
+type postcondition  = trace list
+
+
+let name_ (lhs: es) (rhs : es list ) : bool = 
+
+
+        ;;
+
+let inclusion (lhs: es list) (rhs : es list ) : bool = 
+        let es_1 = forall es from lhs
+        name_ es_1 rhs = true 
+
+        ;;
+
+
+
+        (*
+  yes:      A |- C /\ B |- C
+        --------------------
+         A \/ B |- C
+
+yes:     A |- C /\ A |- B
+        --------------------
+         A  |- C /\  B
+
+
+  no:
+
+        A |- C /\ A |- B
+        --------------------
+         A  |- C \/  B
+
+         a > 0 |- a = 1 /\ a > 0 |- a > 1   -> false 
+         -----------------
+         a >0 |- a = 1 \/ a > 1     -> true 
+
+
+
+        a = 1 |- a > 0  /\ a=2 |- a > 0 
+        ----------------
+        a = 1 \/ a= 2 |- a >0 
+
+        a = 1 |\- a > 1  /!\ a=2 |- a > 1
+        ----------------
+        a = 1 \/ a= 2 |- a >1
+        
+        
+        *)
+
 
 type prog = Nothing 
           | Pause 
