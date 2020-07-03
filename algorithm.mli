d39b19b6f57b8c195561788c34befc9629930b33
jcjxwy <jcjxwy@gmail.com>
2020-07-03 15:26:58 +0800

    algorithm for new system

--- algorithm.mli
+++ algorithm.mli
@@ -2,56 +2,47 @@
 open Base;;
 open Core;;
 
-type signal = string;;
 type var = string;;
+type name = string;;
 
-type ss = signal list;;
+type state = One | Zero;;
+type mapping = (var * state);;
+
+type instance = mapping list * mapping list;; 
 
 type es = Bot 
         | Emp 
-        | Instance of ss 
-        | Or of es * es 
+        | Instance of instance 
         | Con of es * es
         | Kleene of es
-        | Time of es * var
 ;;
-
-type terms = Var of var   
-           | Number of int 
-           | Plus of terms * terms
-           | Minus of terms * terms     
-;;
-type pure = TRUE
-          | FALSE
-          | Gt of terms * terms
-          | Lt of terms * terms
-          | GtEq of terms * terms
-          | LtEq of terms * terms
-          | Eq of terms * terms
-          | PureOr of pure * pure
-          | PureAnd of pure * pure
-          | Neg of pure
-;;
-
-type effect = (pure * es) list;; 
+type history = es;;
+type current = mapping list;;
+type trace = history * current;;
+type precondition = var list *  trace;;
+type postcondition  = trace list;;
 
 type inclusion =
-  INC of es * es
+  INC of es list * es list
 ;;
 
-let rec nullable e =
-  match e with
+let rec nullable_single (i:es) : bool =
+  match i with
     |Instance(_) -> false
     |Emp -> true
-    |Or(a, b) -> nullable a || nullable b
-    |Con(a, b) -> nullable a && nullable b
+    |Con(a, b) -> nullable_single a && nullable_single b
     |Kleene(_) -> true
     |Bot -> false
-    |Time(a, b) -> nullable a
 ;;
 
-let rec join list1 list2 =
-  let rec check_repeated t list =
+let rec nullable (e:es list) : bool=
+  match e with
+    |hd::tl -> nullable_single hd || nullable tl
+    |[] -> false
+;;
+
+let rec join (list1:name list list) (list2:name list list) : name list list=
+  let rec check_repeated (t:name list) (list: name list list) : bool =
     match list with
       |hd::tl -> if hd = t then true else check_repeated t tl
       |[] -> false
@@ -61,66 +52,95 @@ let rec join list1 list2 =
   |[] -> list2
 ;;
 
-let rec find_first_element e =
-  match e with
-    |Instance(e) -> e::[]
-    |Con(a, b) -> if nullable a then join (find_first_element a) (find_first_element b) 
-      else find_first_element a
-    |Kleene(e) -> find_first_element e
-    |Or(a, b) -> join (find_first_element a) (find_first_element b)
-    |_ -> []
+let rec join_single (list1:es list) (list2:es list) : es list=
+  let rec check_repeated (t:es) (list:es list) : bool =
+    match list with
+      |hd::tl -> if hd = t then true else check_repeated t tl
+      |[] -> false
+  in 
+  match list1 with
+  |hd::tl -> if check_repeated hd list2 then join_single tl list2 else hd::join_single tl list2
+  |[] -> list2
 ;;
 
-let rec contains ss1 ss2 =
-  let rec contain_single s ss =
+let rec rewrite (i:mapping list) : name list=
+  match i with
+    |hd::tl -> let rewrite_single (e:mapping) : name =
+      match e with (a,b) -> if b = One then a else "None"
+    in let result = rewrite_single hd in
+      if result = "None" then rewrite tl else result::rewrite tl
+    |[] -> []
+;;
+
+let rec find_first_element (e:es list) : name list list =
+  let rec find_first_element_single (i:es) : name list list =
+    match i with
+      |Instance(a,b) -> [rewrite b]
+      |Con(a, b) -> if nullable_single a then join (find_first_element_single a) (find_first_element_single b)
+        else find_first_element_single a
+      |Kleene(a) -> find_first_element_single a
+      |_ -> []
+  in match e with
+    |hd::tl -> let rec remove_empty (i:name list list) : name list list = 
+      match i with 
+        |hd::tl -> if hd = [] then tl else hd::remove_empty tl
+        |[] -> [] 
+    in remove_empty (join (find_first_element_single hd) (find_first_element tl))
+    |[] -> []
+;;
+
+let rec contains (ss1:name list) (ss2:name list) : bool =
+  let rec contain_single (s:name) (ss:name list) : bool =
     match ss with 
       |hd::tl -> if s = hd then true else contain_single s tl
       |[] -> false
   in match ss2 with
-    |hd::tl -> if contain_single hd ss1 then contains ss1 tl else false
+    |hd::tl -> if contain_single hd ss1 then contains ss1 tl 
+      else false
     |[] -> true
 ;;
 
-let rec unfold element expr =
-  match expr with
-    |Instance(s) -> if contains element s then Emp else Bot
-    |Con(a,b) -> if nullable a then Or(Con(unfold element a, b), unfold element b)
-      else Con(unfold element a, b)
-    |Or(a,b) -> Or(unfold element a, unfold element b)
-    |Emp -> Bot
-    |Bot -> Bot
-    |Kleene(s) -> Con(unfold element s, expr)
-    |_ -> expr
+let rec unfold (element:name list) (expr:es list) : es list =
+  let rec flatten (a:es list) (b:es) : es list =
+    match a with
+      |hd::tl -> Con(hd, b)::(flatten tl b)
+      |[] -> []
+  in let rec unfold_single (element:name list) (e:es) : es list =
+    match e with
+      |Instance(a, b) -> let result = rewrite b in 
+        if result = [] then [Bot]
+        else if contains element (rewrite b) then [Emp] 
+        else [Bot]
+      |Con(a,b) -> if nullable_single a then join_single (flatten (unfold_single element a) b) (unfold_single element b)
+        else flatten (unfold_single element a) b
+      |Emp -> [Bot]
+      |Bot -> [Bot]
+      |Kleene(s) -> flatten (unfold_single element s) e
+  in match expr with
+    |hd::tl -> join_single (unfold_single element hd) (unfold element tl)
+    |[] ->[]
 ;;
 
-let rec is_repeated_term plus term =
-  match plus with
-    |Or(a,b) -> if a = term || b = term then true else is_repeated_term a term || is_repeated_term b term
-    |_ -> false
+let rec normalize (e:es list) : es list =
+  let rec normalize_single (i:es) : es =
+    match i with
+      |Con(a, b) -> if a = Emp || normalize_single a = Emp then normalize_single b
+        else if b = Emp || normalize_single b = Emp then normalize_single a
+        else if a = Bot || normalize_single a = Bot || b = Bot || normalize_single b = Bot then Bot
+        else Con(normalize_single a, normalize_single b)
+      |_ -> i
+  in match e with 
+    |hd::tl -> (normalize_single hd)::(normalize tl)
+    |[] -> []
 ;;
 
-let rec normalize e =
-  match e with
-    |Con(a, b) -> if a = Emp || normalize a = Emp then normalize b
-      else if b = Emp || normalize b = Emp then normalize a
-      else if a = Bot || normalize a = Bot || b = Bot || normalize b = Bot then Bot
-      else Con(normalize a, normalize b)
-    |Or(a,b) -> if a = Bot || normalize a = Bot then normalize b
-      else if b = Bot || normalize b = Bot then normalize a
-      else if is_repeated_term a b then normalize a
-      else if is_repeated_term b a then normalize b
-      else if a = b then normalize a
-      else Or(normalize a, normalize b)
-    |_ -> e
-;;
-
-let rec check_include i memory =
+let rec check_include (i:inclusion) (memory:inclusion list) : bool =
   match memory with
     |hd::tl -> if hd = i then true else check_include i tl
     |[] -> false
 ;;
 
-let rec evaluate elements memory lhs rhs =
+let rec evaluate elements memory (lhs:es list) (rhs:es list) : bool =
   match elements with
     |hd::tl -> let dev_lhs = normalize (unfold hd lhs) and dev_rhs = normalize (unfold hd rhs) in
       let null_lhs = nullable dev_lhs and null_rhs = nullable dev_rhs and i = INC(dev_lhs, dev_rhs) in
@@ -131,8 +151,9 @@ let rec evaluate elements memory lhs rhs =
     |[] -> if nullable lhs && not (nullable rhs) then false else true
 ;;
 
-let check_containment lhs rhs =
-  if nullable lhs && not (nullable rhs) then false
-  else let elements = find_first_element lhs in
-      evaluate elements [INC(lhs, rhs)] (normalize lhs) (normalize rhs)
+let rec check_containment (lhs:es list) (rhs:es list) : bool =
+  let l = normalize lhs and r = normalize rhs in 
+    if nullable l && not (nullable r) then false
+    else let elements = find_first_element l in
+      evaluate elements [INC(l, r)] l r
 ;;
