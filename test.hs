fe996cc987cf997ac673933dc5b3a9c5a942db51
songyahui <songyahuiapp@hotmail.com>
2021-11-24 15:21:30 +0800

    m

--- test.hs
+++ test.hs
@@ -21,12 +21,24 @@ twice g x = g (g x)
               , quadpost :: a -> a -> Bool
               >.
               
-        {|- }
+        {x::a<twicepre> -> <twicepost x>}
         {f::(z:a<fpre> -> a<fpost z>) |- (z:a<fpre> -> a<fpost z>) <: (z:a<twicepre> -> a<twicepre z>)}
        f:(z:a<fpre> -> a<fpost z>)
     -> x:a<quadpre> -> a<quadpost x>
 @-}
-quad f x = 
-    let g = twice twice f 
-    in g x
- 
+quad f x = let g = twice twice f in g x
+-- twice <: twice.g.pre -> twice.g.post
+
+
+{-@ goo :: x:{v:Nat | v < 101} -> {v:Nat | v < 99} @-}
+goo :: Int -> Int
+goo x = 20
+
+{-@ koo :: (x:{v:Nat | v < 100} -> {v:Nat | v < 100}) -> {v:Nat | v < 100} -> Nat @-}
+koo :: (Int -> Int) -> Int -> Int
+koo g x = g x
+
+test = koo goo 0
+
+-- goo real <: goo formal
+-- (x:{v:Nat | v < 101} -> {v:Nat | v < 99}) <: (x:{v:Nat | v < 100} -> {v:Nat | v < 100})
